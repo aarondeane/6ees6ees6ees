fab2ecbbff48bafb8adddeb03cbc9b325a0e33ab
Aaron Deane <aaronmdeane@gmail.com>
2018-11-12 13:20:14 -0800

    Completed 6ee6ee6ees with ES6 syntax

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
-class ForagerBee {
+class ForagerBee extends HoneyMakerBee {
   // TODO..
+  constructor() {
+    super()
+    this.job = 'find pollen'
+    this.canFly = true
+    this.treasureChest =[]
+  }
+
+  forage(treasure) {
+    this.treasureChest.push(treasure)
+  }
+
 };
